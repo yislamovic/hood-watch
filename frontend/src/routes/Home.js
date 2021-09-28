a24e5fa4b6dca20be439aefca3d2c2fe121c61a7
aphy18 <aphason1@gmail.com>
2021-09-28 17:06:43 -0400

    working on home page

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,9 +1,26 @@
+import "../styles/Home.css";
+
 function Home() {
 
   return (
     <>
-    <h3>Home Page</h3>
-    <div></div>
+    <div class="home-container">
+    <h3>Welcome to Neighbourhood Watch</h3>
+      <div class="about-container">
+        
+       <p>Welcome to the neighbourhood watch 👀 🦉 ! Our app is dedicated to creating more safe and positive space by informing users of many good/concerning things that may take place in their areas ✅ . Please feel free to report anything that may take place in your community 📝 . If you have something to report, feel free to <b><a href="/register">Register Here</a></b>. If you already have an account you can <b><a href="/login">Login Here</a></b>. Our app is continuosly growing and we would love your feeback 📈 . Feel feel to email us at aphason1@gmail.com if you have any burning questions 🔥 . Enjoy your say and thanks for checking us out! Stay watchin' folks!!! 🔭 </p>
+      </div>
+
+      <div class="home-ref">
+      <div class="heatmap-box">
+       <p>Talk About Heatmap</p>
+      </div>
+      <div class="report-box">
+      <p>Talk About Reports</p>
+      </div>
+      </div>
+      <div class="home-footer"></div>
+    </div>
     </>
   );
 }
