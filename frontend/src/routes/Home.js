2a01f5b24e658300fc812578a4ca2773859a681b
aphy18 <aphason1@gmail.com>
2021-09-28 19:31:30 -0400

    more formatting, change to className instead of class

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -4,22 +4,33 @@ function Home() {
 
   return (
     <>
-    <div class="home-container">
-    <h3>Welcome to Neighbourhood Watch</h3>
-      <div class="about-container">
+    <div className="home-container">
+    <div className="home-header">
+      <div className="home-title">
+     <h1>Welcome to Neighbourhood Watch</h1>
+     </div>
+      <div className="about-container">
+        <p>Welcome to the neighbourhood watch 👀 🦉 ! Our app is dedicated to creating more safe and positive space by informing users of many good/concerning things that may take place in their areas ✅ . Please feel free to report anything that may take place in your community 📝 . If you have something to report, feel free to <b><a href="/register">Register Here</a></b>. If you already have an account you can <b><a href="/login">Login Here</a></b>. Our users are slowly growing in number and we would love your feeback 📈 . Feel feel to email us at <b>AskTheWatchers3@gmail.com</b> if you have any burning questions 🔥 . Enjoy your say and thanks for checking us out! Stay watchin' folks!!! 🔭 </p>
+      </div>
+    </div>
+    
+      <div className="home-ref">
         
-       <p>Welcome to the neighbourhood watch 👀 🦉 ! Our app is dedicated to creating more safe and positive space by informing users of many good/concerning things that may take place in their areas ✅ . Please feel free to report anything that may take place in your community 📝 . If you have something to report, feel free to <b><a href="/register">Register Here</a></b>. If you already have an account you can <b><a href="/login">Login Here</a></b>. Our app is continuosly growing and we would love your feeback 📈 . Feel feel to email us at aphason1@gmail.com if you have any burning questions 🔥 . Enjoy your say and thanks for checking us out! Stay watchin' folks!!! 🔭 </p>
+        <div className="visit-header">
+        <h2>Explore</h2>
+        </div>
+
+      <div className="visit-container">
+      <div className="report-box">
+      <p>Click <a href="/new">here</a> to post a report that everyone can see!</p>
+      </div>
+      <div className="heatmap-box">
+       <p>Click <a href="/map">here</a> view a map that highlights areas as 'safe' or 'concerning' based on the reports that've been sent in.</p>
+      </div>
       </div>
 
-      <div class="home-ref">
-      <div class="heatmap-box">
-       <p>Talk About Heatmap</p>
       </div>
-      <div class="report-box">
-      <p>Talk About Reports</p>
-      </div>
-      </div>
-      <div class="home-footer"></div>
+      <div className="home-footer"></div>
     </div>
     </>
   );
