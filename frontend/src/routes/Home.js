ffa1c5ea28e8da3aa790c9400ffa363bd859b382
aphy18 <aphason1@gmail.com>
2021-09-28 22:15:23 -0400

    refining colours of home page

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,5 +1,5 @@
 import "../styles/Home.css";
-import neighbourhoodImage from "../assets/neighbourhood.png"
+import neighbourhoodImage from "../assets/cartoon-street.jpeg"
 function Home() {
 
   return (
@@ -11,7 +11,7 @@ function Home() {
      </div>
 
       <div className="about-container">
-        <p>Welcome to the neighbourhood watch 👀 🦉 ! Our app is dedicated to creating more safe and positive space by informing users of many good/concerning things that may take place in their areas ✅ . Please feel free to report anything that may take place in your community 📝 . If you have something to report, feel free to <b><a href="/register">Register Here</a></b>. If you already have an account you can <b><a href="/login">Login Here</a></b>. Our users are slowly growing in number and we would love your feeback 📈 . Feel feel to email us at <b>AskTheWatchers3@gmail.com</b> if you have any burning questions 🔥 . Enjoy your say and thanks for checking us out! Stay watchin' folks!!! 🔭 </p>
+        <p>Welcome to the neighbourhood watch 🕵 🦉 ! Our app is dedicated to creating more safe and positive space by informing users of many good/concerning things that may take place in their areas ✅ . Please feel free to report anything that may take place in your community 📝 . Feel free to <b><a href="/register">Register Here</a></b> to create an account. If you already have an account you can <b><a href="/login">Login Here</a></b>. Our users are slowly growing in number and we would love your feeback 📈 . Feel feel to email us at <b>AskTheWatchers3@gmail.com</b> if you have any burning questions 🔥 . Enjoy your say and thanks for checking us out! Stay watchin' folks!!! 👁 👁  </p>
       </div>
 
     </div>
