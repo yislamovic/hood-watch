7a4a9297c0b652ea0ebcd884ef7ac773e20b140b
aphy18 <aphason1@gmail.com>
2021-09-29 18:09:45 -0400

    implemented char counter

--- frontend/src/hooks/useForm.js
+++ frontend/src/hooks/useForm.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 export default function useForm(callback){
     const [values,setValues] = useState({})
+
     const handleChange = e => {
         // grabbing the values of the inputs we put in, this works because of the 'name' in the register form
         const { name, value } = e.target
@@ -10,7 +11,7 @@ export default function useForm(callback){
         console.log("values -->", values)
         setValues({
             ...values,
-            [name]:value
+            [name]:value,
         });
     };
     
--- frontend/src/routes/New.js
+++ frontend/src/routes/New.js
@@ -3,12 +3,35 @@ import useForm from "../hooks/useForm";
 import { useState, useEffect } from 'react';
 
 export default function New(){
-    const { handleChange, handleSubmit, values, setValue} = useForm(handleNewPost) 
+    const { handleChange, handleSubmit, values, setValues } = useForm(handleNewPost)
+    const [textLength, setTextLength] = useState(0)
+    const [isDisabled, setIsDisabled] = useState(true)
+    const [errors, setErrors] = useState({})
 
+    // the callback function is already doing our console.logs
     function handleNewPost(){
-    
-       console.log("values 10 -->", values)
+        if (values.title.length < 5) {
+           return setErrors({ title: "Title must be more than 5 characters"})
+       }
+       if (values.post.length > 20) {
+        return setErrors({ post: "Description must be less than 20 characters"})
     }
+       
+       setValues({})
+       console.log("Form Submitted")
+    }
+
+    useEffect(() => {
+      if (Object.values(values).filter(value => value !== "").length === 3) {
+        setIsDisabled(false)
+      } else {
+        setIsDisabled(true)
+      }
+      if (values.post) {
+        setTextLength(values.post.length)
+      }
+    }, [values])
+
 
     return (
         <>
@@ -20,29 +43,35 @@ export default function New(){
              <input type="text"
              name="title" 
              placeholder="Title of Report"
+             value={values.title}
              onChange={handleChange}
              required
              >
              </input>
+             {errors.title && <p className="error-message">{errors.title}</p>}
            </div>
 
          <div className="form-group">
-             <p>Description: </p>
+             <label>{textLength}/500</label>
+             <p>Post: </p>
             <textarea
             className="description-box"
-             name="description"
+             name="post"
              rows="10"
              cols="10"
              placeholder="Insert Description"
+             value={values.post}
              onChange={handleChange}
              required
             >
             </textarea>
+             <p></p>
+            {errors.post && <p className="error-message">{errors.post}</p>}
             </div>
 
          <div className="form-group">
             <label>Category: </label>
-            <select name="category" onChange={handleChange} required>
+            <select name="category" value={values.category} onChange={handleChange} required>
               <option value="trending">Trending</option>
               <option value="celebrate">Celebrate</option>
               <option value="caution">Cautio</option>
@@ -51,9 +80,10 @@ export default function New(){
               <option value="community-question">Question</option>
               <option value="other">Other</option>
           </select>
+            {errors.category && <p className="error-message">{errors.category}</p>}
          </div>
          <div className="button-container">
-          <button type="submit" className="btn">Post</button>
+          <button type="submit" className="btn" disabled={isDisabled}>Post</button>
         </div>
           </form>
           </div>
