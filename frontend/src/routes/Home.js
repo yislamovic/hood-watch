4503ba5e6556726cfebb6e4120018733a93f3f3f
aphy18 <aphason1@gmail.com>
2021-09-28 20:12:23 -0400

    background image on home page

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,17 +1,19 @@
 import "../styles/Home.css";
-
+import neighbourhoodImage from "../assets/neighbourhood.png"
 function Home() {
 
   return (
     <>
     <div className="home-container">
-    <div className="home-header">
+    <div className="home-header" style={{ backgroundImage: `url(${neighbourhoodImage})`}}>
       <div className="home-title">
      <h1>Welcome to Neighbourhood Watch</h1>
      </div>
+
       <div className="about-container">
         <p>Welcome to the neighbourhood watch 👀 🦉 ! Our app is dedicated to creating more safe and positive space by informing users of many good/concerning things that may take place in their areas ✅ . Please feel free to report anything that may take place in your community 📝 . If you have something to report, feel free to <b><a href="/register">Register Here</a></b>. If you already have an account you can <b><a href="/login">Login Here</a></b>. Our users are slowly growing in number and we would love your feeback 📈 . Feel feel to email us at <b>AskTheWatchers3@gmail.com</b> if you have any burning questions 🔥 . Enjoy your say and thanks for checking us out! Stay watchin' folks!!! 🔭 </p>
       </div>
+
     </div>
     
       <div className="home-ref">
