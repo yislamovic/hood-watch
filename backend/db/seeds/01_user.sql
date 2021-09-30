2565d9b97773bcc0174e3781cb192cf0678ab75f
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-09-30 18:23:29 -0400

    changes to seeds

--- backend/db/seeds/01_user.sql
+++ backend/db/seeds/01_user.sql
@@ -1,13 +1,13 @@
 INSERT INTO user (first_name, last_name, email, user_password, user_address)
-VALUES ('Jeff', 'Baker', 'jeff@gmail.com', '1234', 'Toronto Ontario, 34 London Dr'),
-('Lonzo', 'Ball', 'baller12@gmail.com','1234', 'Toronto Ontario, 86 Spadina Av '),
-('Hot', 'Coco', 'chocolate@gmail.com','1234', 'Toronto Ontario, 45 Rosedale Blvd'),
-('Reef', 'Monico', 'baller12@gmail.com','1234', 'Toronto Ontario, 22 Denision Av'),
-('Linsday', 'Hustler', 'immahustle2day@gmail.com','1234', 'Toronto Ontario, 405 Jane Western Rd'),
-('Andy', 'Love', 'loveguyman@gmail.com', '1234', 'Toronto Ontario, 303 Spadina Av'),
-('Detray', 'Anderson', 'thatboyhoop23@gmail.com', '1234', 'Toronto Ontario, 16 Lord Landsdowne Way');
-('Brandon','Xang', 'brandon256@gmail.com', '1234', 'Toronto Ontario, 700 Dufferin St')
-('Kiran','Waterson', 'ilovewater@gmail.com', '1234', 'Toronto Ontario, 11 Waterfront St')
-('Dilan','Summers', 'd483@gmail.com', '1234', 'Toronto Ontario, 12 College St')
-('Rose','Amysetrich', 'summergirl111@gmail.com', '1234', 'Toronto Ontario, High Park')
-('Aphason','Aberham','aphason1@gmail.com','1234','Toronto Ontario Bloor St E')
+VALUES ('Jeff', 'Baker', 'jeff@gmail.com', '1234', 'Toronto, Ontario, 34 London Dr'),
+('Lonzo', 'Ball', 'baller12@gmail.com','1234', 'Toronto, Ontario, 86 Spadina Av '),
+('Hot', 'Coco', 'chocolate@gmail.com','1234', 'Toronto, Ontario, 45 Rosedale Blvd'),
+('Reef', 'Monico', 'baller12@gmail.com','1234', 'Toronto, Ontario, 22 Denision Av'),
+('Linsday', 'Hustler', 'immahustle2day@gmail.com','1234', 'Toronto, Ontario, 405 Jane Western Rd'),
+('Andy', 'Love', 'loveguyman@gmail.com', '1234', 'Toronto, Ontario, 303 Spadina Av'),
+('Detray', 'Anderson', 'thatboyhoop23@gmail.com', '1234', 'Toronto, Ontario, 16 Lord Landsdowne Way');
+('Brandon','Xang', 'brandon256@gmail.com', '1234', 'Toronto, Ontario, 700 Dufferin St')
+('Kiran','Waterson', 'ilovewater@gmail.com', '1234', 'Toronto, Ontario, 11 Waterfront St')
+('Dilan','Summers', 'd483@gmail.com', '1234', 'Toronto, Ontario, 12 College St')
+('Rose','Amysetrich', 'summergirl111@gmail.com', '1234', 'Toronto, Ontario, High Park')
+('Aphason','Aberham','aphason1@gmail.com','1234','Toronto, Ontario Bloor St E')
