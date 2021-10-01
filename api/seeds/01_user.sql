8e8426119779e58959fbdbdac7c007f63c3ccb4b
aphy18 <aphason1@gmail.com>
2021-10-01 14:03:47 -0400

    seeds in api folder

--- api/seeds/01_user.sql
+++ api/seeds/01_user.sql
@@ -5,9 +5,9 @@ VALUES ('Jeff', 'Baker', 'jeff@gmail.com', '1234', 'Toronto, Ontario, 34 London 
 ('Reef', 'Monico', 'baller12@gmail.com','1234', 'Toronto, Ontario, 22 Denision Av'),
 ('Linsday', 'Hustler', 'immahustle2day@gmail.com','1234', 'Toronto, Ontario, 405 Jane Western Rd'),
 ('Andy', 'Love', 'loveguyman@gmail.com', '1234', 'Toronto, Ontario, 303 Spadina Av'),
-('Detray', 'Anderson', 'thatboyhoop23@gmail.com', '1234', 'Toronto, Ontario, 16 Lord Landsdowne Way');
-('Brandon','Xang', 'brandon256@gmail.com', '1234', 'Toronto, Ontario, 700 Dufferin St')
-('Kiran','Waterson', 'ilovewater@gmail.com', '1234', 'Toronto, Ontario, 11 Waterfront St')
-('Dilan','Summers', 'd483@gmail.com', '1234', 'Toronto, Ontario, 12 College St')
-('Rose','Amysetrich', 'summergirl111@gmail.com', '1234', 'Toronto, Ontario, High Park')
-('Aphason','Aberham','aphason1@gmail.com','1234','Toronto, Ontario Bloor St E')
+('Detray', 'Anderson', 'thatboyhoop23@gmail.com', '1234', 'Toronto, Ontario, 16 Lord Landsdowne Way'),
+('Brandon','Xang', 'brandon256@gmail.com', '1234', 'Toronto, Ontario, 700 Dufferin St'),
+('Kiran','Waterson', 'ilovewater@gmail.com', '1234', 'Toronto, Ontario, 11 Waterfront St'),
+('Dilan','Summers', 'd483@gmail.com', '1234', 'Toronto, Ontario, 12 College St'),
+('Rose','Amysetrich', 'summergirl111@gmail.com', '1234', 'Toronto, Ontario, High Park'),
+('Aphason','Aberham','aphason1@gmail.com','1234','Toronto, Ontario Bloor St E');
--- api/seeds/02_report.sql
+++ api/seeds/02_report.sql
@@ -6,7 +6,7 @@ VALUES
 
 ("Latest Construction News", "2020-12-6 9:15:41", "Everyone in the North York Area, the housing faculty of City Hall estimated that construction will continue for the next 3 months. You should consider taking the express route to work due to traffic on the main route to downtown. Hopefully for Christmas Santa will make the process go by quicker.", "108-4400 Jane St, Toronto ON M3N 2K4", 5),
 
-("Ryerson Community School BakeSale", "2021-3-11 11:13:20", "I was walking by the area and I saw some elementary students selling cupcakes. I decided to support their fundraiser, so I bought some. I must say that they were delicious!! The chocolate cupcake with carrot caramel on top was my personal favourite!! Keep up good, and I hope you reach your goal :)", "96 Denison Ave, Toronto, ON M5T 1E4", 4),
+("Ryerson Community School Bakesale", "2021-3-11 11:13:20", "I was walking by the area and I saw some elementary students selling cupcakes. I decided to support their fundraiser, so I bought some. I must say that they were delicious!! The chocolate cupcake with carrot caramel on top was my personal favourite!! Keep up good, and I hope you reach your goal :)", "96 Denison Ave, Toronto, ON M5T 1E4", 4),
 
 ("Stanely Park Basketball Tournament", "2021-6-25 5:30:01", "Anyone in the area that wants to participate in a 5v5 basketball tournament can come down next week to Stanley Park. Signups are available on our website, and we will be providing pizza and prizes!!! Whether you want to participate or just come down to watch, this is a great event to bring people together.", "890 King St W, Toronto, ON M5V 1P2", 2),
 
@@ -16,5 +16,5 @@ VALUES
 
 ("ITS ALEXANDRA PARKS BIRTHDAY 🎂 🎊 🎊","2021-8-19 00:00:00","Lets us celebrate 46 years of growth, community, and prosperity within our neighbourhood. We, at the community services, would like to thank all of the workers, volunteers, and committees who've helped nurture the Alexandra Park Community!!!!! Let us continue to move forward while keeping our rich history in both mind and heart..Bless you all.❤️‍🔥","105 Grange Ct, Toronto, ON M5T 2J6", 6),
 
-("What is it like choking?", "2021-8-23 10:10:10","It can't be just me that's wondering what choking is like?Common...anyone, enlighten me?", "801 Bay St Ste 1, Toronto ON M5S1Y9",10);
+("What is it like choking?", "2021-8-23 10:10:10","It can't be just me that's wondering what choking is like? Common...anyone, enlighten me?", "801 Bay St Ste 1, Toronto ON M5S1Y9",10);
 
