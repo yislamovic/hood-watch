382e08173b605fe69db554c5acd094227d0d5be7
Arian Abedanzadeh <arianabedan@gmail.com>
2021-10-03 18:46:33 -0400

    Merge Error

--- frontend/src/component/Nav.js
+++ frontend/src/component/Nav.js
@@ -1,11 +1,34 @@
 import { Link } from 'react-router-dom'
+import React, { useState } from "react";
 import Image from '../assets/logo.png'
 function Nav() {
   const navStyle = {
     color: 'white'
   };
-  return (
-    <nav>
+  const [email] = useState("");
+  const [user] = useState()
+
+  if (user) {
+    return (
+      <nav> 
+        <img src={Image} alt="logo" width="80px" height="56px"/>
+        <ul className='nav-links'>
+          <Link style={navStyle} to='/'>
+            <li>Home</li>
+          </Link>
+          <Link style={navStyle} to='/logout'>
+          <div>{email} is loggged in</div>;
+            <li>Logout</li>
+          </Link>
+          <Link style={navStyle} to='/register'>
+            <li>Register</li>
+          </Link>
+        </ul>
+    </nav>
+    );
+  } else {
+    return (
+      <nav> 
         <img src={Image} alt="logo" width="80px" height="56px"/>
         <ul className='nav-links'>
           <Link style={navStyle} to='/'>
@@ -19,7 +42,10 @@ function Nav() {
           </Link>
         </ul>
     </nav>
-  );
+    );
+    console.log(email);
+  }
 }
+
 
 export default Nav;
