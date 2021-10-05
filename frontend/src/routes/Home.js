070f5e50e3c90a628bd90c2e0c23bd684f54b035
aphy18 <aphason1@gmail.com>
2021-10-05 19:11:17 -0400

    about us refinement

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -18,19 +18,19 @@ function Home() {
       </div>
       <div className="space-text-container">
       <div className="mini-space-text-container-1">
-       <span className="space-text" id="space-text-1">We want to create more safe and positive spaces</span>
+       <span className="space-text" id="space-text-1"><b>Our app is...</b></span>
        <span className="pink-line"></span>
-       <span className="space-text" id="space-text-2">Want to Unite Communities </span>
+       <span className="space-text" id="space-text-2">Designed to join all our communities together!</span>
       </div>
       <div className="mini-space-text-container-2">
-       <span className="space-text" id="space-text-3">We serve our communities</span>
+       <span className="space-text" id="space-text-3"><b>Our mission is...</b></span>
        <span className="pink-line"></span>
-       <span className="space-text" id="space-text-4">Like and Subscribe</span> 
+       <span className="space-text" id="space-text-4">To create a safe enviroment to report anything in!</span> 
        </div>
        <div className="mini-space-text-container-3">
-       <span className="space-text" id="space-text-5">We serve our communities</span>
+       <span className="space-text" id="space-text-5"><b>We greatly appreciate...</b></span>
        <span className="pink-line"></span>
-       <span className="space-text" id="space-text-6">Like and Subscribe</span> 
+       <span className="space-text" id="space-text-6">Your constant support and user feedback!</span> 
        </div>
        </div>
 
