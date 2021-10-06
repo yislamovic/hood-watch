9cdda32ed6b9c10868a0d4ca6f4ff37912275e35
aphy18 <aphason1@gmail.com>
2021-10-06 07:02:25 -0400

    found the right console log

--- frontend/src/component/LikedByContainer.js
+++ frontend/src/component/LikedByContainer.js
@@ -5,22 +5,18 @@ import "../styles/ButtonGroup.css"
 import { useState, useEffect } from 'react';
 import axios from "axios";
 import LikedBy from "./LikedBy";
+
+
+
+
+
 function LikedByContainer() {
  
   return (
     <>
-    <div className="post-footer-container">
-    <div className="vote-title-container">
-    <h2>Upvote/Downvote</h2>
-    </div>
-      <div className="liked-by">
+     <div className="liked-by">
         <LikedBy />
       </div>
-      
-        {/* <img className="upvote" onClick={() => upVoteBool && upVote()} src={Image} alt="UpVote" />
-        <img className="downvote" onClick={() => downVoteBool && downVote()} src={Image} alt="DownVote" /> */}
-     
-    </div>
   </>
   );
 }
